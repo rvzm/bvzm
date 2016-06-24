eee7e2bc0ac8916ccc2d43d969e966bf69fe747a
rvzm <rvxm@outlook.com>
2016-06-23 23:11:15 -0800

    Minor updates

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# bvzm.tcl #
- Current Stable \- 0.1
- Current Dev    \- 0.1.2
+# bvzm.tcl \- version 0.2
 
 ## !!REQUIRED!!
 This is a [eggdrop](http://eggheads.org) 1.8.x script. So far it requires no extra packages be installed.
--- bvzm.tcl
+++ bvzm.tcl
@@ -2,15 +2,14 @@
 # ### bvzm.tcl - bvzm tool file ####################
 # ### Coded by rvzm             ####################
 # ### ------------------------- ####################
-# ### Edited: 6/21/2016         ####################
-# ### Version: 0.1              ####################
+# ### Edited: 05/31/2016        ####################
+# ### Version: 0.2              ####################
 # ##################################################
 
 set pubtrig "@"
 set controller "~"
 set bvzm_dir "bvzm_db"
-set npass ""
-
+set controlchan "#logs"
 # some flags
 setudef flag bvzm
 setudef flag avoice
@@ -19,20 +18,20 @@ proc getTrigger {} {
   global pubtrig
   return $pubtrig
 }
-proc getPass {} {
-	global npass
-	return $npass
-}
+
 # Main Commands
 bind pub - ${pubtrig}bvzm bvzm:main
-bind pub - ${pubtrig}pack weed:pack
 bind pub - ${pubtrig}boobies yus:boobies
 # Friendly commands
-bind pub f ${pubtrig}mvoice mass:voice
 bind pub f ${pubtrig}chk sym:check
+bind pub f ${pubtrig}rollcall nicks:rollcall
+bind pub f ${pubtrig}pack weed:pack
+# Op commands
+bind pub o ${pubtrig}mvoice hub:mvoice
 # Control Commands
 bind pub m ${controller}bvzm hub:control
-bind pub m ${pubtrig}rollcall nicks:rollcall
+# In-progress Commands
+bind pub m ${controller}status bvzm:status
 
 # Autos
 bind join - * hub:autovoice
@@ -41,20 +40,20 @@ bind join - * hub:autovoice
 if {![file exists $bvzm_dir]} {
 	file mkdir $bvzm_dir
 }
-proc write_bdb { w_db w_info } {
+proc concw_db { w_db w_info } {
 	global bvzm_dir
 	set fs_write [open $bvzm_dir/$w_db w]
 	puts $fs_write "$w_info"
 	close $fs_write
 }
-proc bdb { r_db } {
+proc conn_db { r_db } {
 	global bvzm_dir
 	set fs_open [open $bvzm_dir/$r_db r]
 	gets $fs_open db_out
 	close $fs_open
 	return $db_out
 }
-proc create_bdb { bdb db_info } {
+proc connc_db { bdb db_info } {
 	global bvzm_dir
 	if {[file exists $bvzm_dir/$bdb] == 0} {
 		set crtdb [open $bvzm_dir/$bdb a+]
@@ -69,21 +68,25 @@ proc yus:boobies {nick uhost hand chan text} {
 	putlog "BOOBIEZ"
 }
 proc weed:pack {nick uhost hand chan text} {
+	global packed
+	if {$packed == "yes"} { return }
 	putserv "PRIVMSG $chan \00303Pack your bowls! Chan-wide Toke-out in 1 Minute!\003"
 	global wchan
-	set wchan $chan
+	set wchan
+	set packed yes
 	utimer 60 weed:pack:go
 }
 proc weed:pack:go {} {
-	global wchan
+	global wchan packed
 	putserv "PRIVMSG $wchan :\00303::\003045\00303:";
 	putserv "PRIVMSG $wchan :\00303::\003044\00303:";
 	putserv "PRIVMSG $wchan :\00303::\003043\00303:";
 	putserv "PRIVMSG $wchan :\00303::\003042\00303:";
 	putserv "PRIVMSG $wchan :\00303::\003041\00303:";
 	putserv "PRIVMSG $wchan :\00303::\00311\002SYNCRONIZED!\002 \00304FIRE THEM BOWLS UP!!!"; return
-
+	set packed no
 }
+
 proc bvzm:main {nick uhost hand chan text} {
   global botnick
     if {[lindex [split $text] 0] == ""} {
@@ -97,9 +100,19 @@ proc bvzm:main {nick uhost hand chan text} {
 	if {[lindex [split $text] 0] == "info"} {
 	  putserv "PRIVMSG $chan :bvzm.tcl - bvzm tool file ~ Coded by rvzm"; return
 	}
+	if {[lindex [split $text] 0] == "quote"} {
+		puthelp "PRIVMSG $chan :Quote Commands (<required> \[optional\])"
+		puthelp "PRIVMSG $chan :!addquote <quote> (adds a quote)"
+		puthelp "PRIVMSG $chan :!delquote <quote id> (deletes a quote)"
+		puthelp "PRIVMSG $chan :!quote \[quote id\] (gets a specific or random quote)"
+		puthelp "PRIVMSG $chan :!findquote <regexp string> (find quotes matching regexp string)"
+		puthelp "PRIVMSG $chan :!lastquote (get the last quote added to the database)"
+		puthelp "PRIVMSG $chan :!quoteauth <nick> (find quotes added by nick)"
+		puthelp "PRIVMSG $chan :!quotechan <#chan> (find quotes added in #chan)"
+	}
 }
-proc mass:voice {nick uhost hand chan text} {
-  if {![matchattr $hand o|m $chan] && ![channel get $chan bvzm]} {return}
+proc hub:mvoice {nick uhost hand chan text} {
+  if {![channel get $chan bvzm]} {return}
     set botnick "bvzm";
     if {[string tolower $nick] != [string tolower $botnick]} {
       foreach whom [chanlist $chan] {
@@ -111,10 +124,14 @@ proc mass:voice {nick uhost hand chan text} {
   }
 }
 proc nicks:rollcall {nick uhost hand chan text} {
-  if {![matchattr $hand o|m $chan] && ![channel get $chan bvzm]} {return}
+  if {![channel get $chan bvzm]} {return}
    set rollcall [chanlist $chan]
    putserv "PRIVMSG $chan :Roll Call!"
    putserv "PRIVMSG $chan :$rollcall"
+}
+proc sym:rekt {nick uhost hand chan text} {
+	putserv "PRIVMSG $chan :\[\u2714\] rekt";
+	return
 }
 proc sym:check {nick uhost hand chan text} {
 	putserv "PRIVMSG $chan :\[\u2714\] $text";
@@ -158,4 +175,13 @@ proc hub:autovoice {nick host hand chan} {
 	}
 }
 
-putlog "bvzm.tcl -- LOADED"
+# --- IN-PROGRESS PROCS ---
+proc bvzm:status {nick uhost hand chan text} {
+	set ulvl [chattr get $nick]
+	set ulvl [chattr get [lindex [split $text] 0]]
+	putlog "user flag query: $nick -> $ulvl"
+	putserv "PRIVMSG $chan user flag query -> $ulvl"
+}
+# --- END IN-PROGRESS PROCS ---
+
+putlog "bvzm.tcl version 0.2 -- LOADED"
