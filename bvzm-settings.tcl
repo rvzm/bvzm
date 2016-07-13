928d10d8295466cc9c36d390dfdfcdc97cc29fe6
rvzm <rvxm@outlook.com>
2016-07-13 02:21:09 -0800

    version update

--- bvzm-settings.tcl
+++ bvzm-settings.tcl
@@ -1,6 +1,6 @@
 namespace eval bvzm {
 	namespace eval settings {
-		variable version "0.3.6"
+		variable version "0.3.7"
 		namespace eval gen {
 			variable pubtrig "@"
 			variable controller "~"
