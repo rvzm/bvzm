6dae31b020bd9a1b51de617a947d833e907ce21b
Connor Angell <rvzm@protonmail.com>
2016-11-13 06:03:08 +0000

    now using official lastfm service, still trying to get it working

--- addons/lastfm.tcl
+++ addons/lastfm.tcl
@@ -8,20 +8,19 @@ namespace eval bvzm {
 			variable trig "-";
 		}
 		package require http
+		package require xml
 		bind pub - ${bvzm::lastfm::settings::trig}np bvzm::lastfm::procs::np
 		bind pub - ${bvzm::lastfm::settings::trig}set bvzm::lastfm::procs::set
 		namespace eval procs {
 			proc np {nick uhost hand chan text} {
-				#putserv "PRIVMSG $chan :bvzm -> lastfm plugin unavailable."; return
-				set arg2 1
-				set arg1 recent
+				#putserv "PRIVMSG $chan :bvzm -> lastfm plugin unavailable."; return				
 				set luser [bvzm::lastfm::procs::getUser $nick]
 				set v1 [lindex [split $text] 0]
 				if {$v1 !== ""} { set arg0 $v1 } else {
 					if {$luser == ""} { putserv "PRIVMSG $chan :error - you need to specify or set your username"; return }
 					set arg0 $luser;
 				}
-				set url "http://forum.teranetworks.de/lastfm2.php?user=$arg0&anzahl=$arg2&modus=$arg1"
+				set url "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$arg0&api_key=df1576f056933fe7c5253397a699da18"
 				http::config -useragent "Mozilla/5.0"
 				set incoming [http::data [http::geturl "$url"] ]
 				http::cleanup $incoming
